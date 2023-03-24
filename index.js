d5ede3f21d54a843d38b484e7bf9111d0e8012fe
prudence <prudence.jerono@student.moringaschool.com>
2023-03-24 12:23:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+const employee = {name: 'Sam', streetAddress: '11 Broadway'
+};
+employee.name = 'Sam'
+employee.streetAddress = '11 Broadway'
+
+const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddresss', '12 Broadway');
+     
+newEmployee.name = 'Sam'
+newEmployee.streetAddress = '12 Broadway'
+
